2cae39283518f073da08a46a6f023f5de22f7709
n8felton <n8felton@gmail.com>
2015-09-14 16:00:59 -0400

    TAB to SPACE and UTF-8 conversion

--- munki-enroll/enroll.php
+++ munki-enroll/enroll.php
@@ -4,14 +4,14 @@ namespace CFPropertyList;
 require_once( 'cfpropertylist-2.0.1/CFPropertyList.php' );
 
 // Get the varibles passed by the enroll script
-$identifier = $_GET["identifier"];
-$hostname   = $_GET["hostname"];
+$identifier         = $_GET["identifier"];
+$hostname           = $_GET["hostname"];
 
 // Split the manifest path up to determine directory structure
-$directories		= explode( "/", $identifier, -1 ); 
-$total				= count( $directories );
-$n					= 0;
-$identifier_path	= "";
+$directories        = explode( "/", $identifier, -1 ); 
+$total              = count( $directories );
+$n                  = 0;
+$identifier_path    = "";
 while ( $n < $total )
     {
         $identifier_path .= $directories[$n] . '/';
