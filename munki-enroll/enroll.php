ad9dc0a1b74d28a162fee6ec7bde71266c25a587
n8felton <n8felton@gmail.com>
2015-09-14 17:04:29 -0400

    Adding support for absolute path to munki repo.
    This requires that the munki repo path be set in
    /Library/Preferences/com.github.munki.plist with the MUNKI_REPO key.

--- munki-enroll/enroll.php
+++ munki-enroll/enroll.php
@@ -3,32 +3,37 @@ namespace CFPropertyList;
 
 require_once( 'cfpropertylist-2.0.1/CFPropertyList.php' );
 
+// Get the munki repo directory
+$plist          = new CFPropertyList( '/Library/Preferences/com.github.munki.plist' );
+$arrPref        = $plist->toArray();
+$munki_repo     = $arrPref['MUNKI_REPO'];
+
 // Get the varibles passed by the enroll script
-$identifier         = $_GET["identifier"];
-$hostname           = $_GET["hostname"];
+$identifier     = $_GET["identifier"];
+$hostname       = $_GET["hostname"];
 
 // Check if manifest already exists for this machine
-if ( file_exists( '../manifests/' . $hostname ) )
+if ( file_exists( $munki_repo . '/manifests/' . $hostname ) )
 {
-	echo "Computer manifest already exists.";
+    echo "Computer manifest already exists.";
 }
 else
 {
-	echo "Computer manifest does not exist. Will create.";
+    echo "Computer manifest does not exist.";
 
-	// Create the new manifest plist
-	$plist = new CFPropertyList();
-	$plist->add( $dict = new CFDictionary() );
+    // Create the new manifest plist
+    $plist = new CFPropertyList();
+    $plist->add( $dict = new CFDictionary() );
 
-	// Add manifest to production catalog by default
-	$dict->add( 'catalogs', $array = new CFArray() );
-	$array->add( new CFString( 'production' ) );
+    // Add manifest to production catalog by default
+    $dict->add( 'catalogs', $array = new CFArray() );
+    $array->add( new CFString( 'production' ) );
 
-	// Add parent manifest to included_manifests to achieve waterfall effect
-	$dict->add( 'included_manifests', $array = new CFArray() );
-	$array->add( new CFString( $identifier ) );
+    // Add parent manifest to included_manifests to achieve waterfall effect
+    $dict->add( 'included_manifests', $array = new CFArray() );
+    $array->add( new CFString( $identifier ) );
 
-	// Save the newly created plist
-	$plist->saveXML( '../manifests/' . $hostname );
+    // Save the newly created plist
+    $plist->saveXML( $munki_repo . '/manifests/' . $hostname );
 }
 ?>
