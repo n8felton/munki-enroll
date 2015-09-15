b33a124a033b20912d77dc13aa0fd837295f93a5
n8felton <n8felton@gmail.com>
2015-09-15 16:43:38 -0400

    Adding support for creating non-existant parent manifests

--- munki-enroll/enroll.php
+++ munki-enroll/enroll.php
@@ -16,6 +16,31 @@ $hostname           = $_GET["hostname"];
 $parent_manifest    = $munki_repo . '/manifests/' . $identifier
 $machine_manifest   = $munki_repo . '/manifests/' . $hostname
 
+// Check if the parent/nested manifest exists
+if ( file_exists( $parent_manifest ) )
+{
+    echo "Parent manifest already exists.";
+}
+else
+{
+    echo "Parent manifest does not exist.";
+
+    // Create the new manifest plist
+    $plist = new CFPropertyList();
+    $plist->add( $dict = new CFDictionary() );
+
+    // Add manifest to production catalog by default
+    $dict->add( 'catalogs', $array = new CFArray() );
+    $array->add( new CFString( 'production' ) );
+
+    // Add parent manifest to included_manifests to achieve waterfall effect
+    $dict->add( 'included_manifests', $array = new CFArray() );
+    $array->add( new CFString( 'site_default' ) );
+
+    // Save the newly created plist
+    $plist->saveXML( $parent_manifest );
+}
+
 // Check if manifest already exists for this machine
 if ( file_exists( $machine_manifest ) )
 {
